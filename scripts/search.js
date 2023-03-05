14e809bb7de2f47a773f85958b7f4c1f89939361
Lorne Cyr <Lorne.S.Cyr@gmail.com>
2023-03-05 14:07:58 -0400

    Refactored javascript and cleaned up html

--- scripts/search.js
+++ scripts/search.js
@@ -1,13 +1,16 @@
 import AnimalsParameters from "./models/animals.js";
 import { fetchAnimals, fetchTypes, fetchBreeds } from "./fetchers.js";
-let submitBtn = document.querySelector("#submit");
+
 let selectInputEls = document.querySelectorAll("#petSelect sl-select");
 let textInputEls = document.querySelectorAll("#petSelect sl-input");
-
+let submitBtn = document.getElementById("submit");
 let locationEl = document.getElementById("location");
 let distanceEl = document.getElementById("distance");
+
 const typesData = await fetchTypes();
 const breedsData = await fetchBreeds("Dog");
+
+renderAllDynamicOptions();
 
 // disable distance field if location is empty
 locationEl.addEventListener("input", function () {
@@ -18,9 +21,7 @@ locationEl.addEventListener("input", function () {
   }
 });
 
-submitBtn.addEventListener("click", buildParams);
-
-renderAllDynamicOptions();
+submitBtn.addEventListener("click", buildApiParams);
 
 function addOptionsToForm(elementId) {
   // get field via ID
@@ -39,9 +40,6 @@ function addOptionsToForm(elementId) {
     let value = elementId === "breed" ? option.name : option;
     let underscored = value.replace(/ /g, "_");
     html += `<sl-option value="${underscored}">${value}</sl-option>`;
-    // TODO: Testing field value -> API call. Remove later.
-    // let newValue = `${inputField.getAttribute("value")} ${underscored}`;
-    // inputField.setAttribute("value", newValue);
   });
 
   inputField.innerHTML = html;
@@ -55,7 +53,7 @@ function renderAllDynamicOptions() {
   });
 }
 
-async function buildParams(event) {
+async function buildApiParams(event) {
   event.preventDefault();
   let paramObj = new AnimalsParameters();
 
@@ -67,11 +65,7 @@ async function buildParams(event) {
         paramObj[value] = true;
       });
     } else {
-      if (option.id === "breed") {
-        paramObj[option.id] = replaceJunkWithSpace(option.value);
-      } else {
-        paramObj[option.id] = replaceJunkWithHyphen(option.value);
-      }
+      paramObj[option.id] = convertToApiCompatible(option.value, option.id);
     }
   }
 
@@ -79,35 +73,33 @@ async function buildParams(event) {
     let inputEl = textInputEls[i];
     paramObj[inputEl.id] = inputEl.value;
   }
-  const testing = await fetchAnimals(paramObj);
-  console.log(testing);
+
+  const results = await fetchAnimals(paramObj);
+  // TODO: Handle no results gracefully.
+  // TODO: prevent double clicking submit button, provide loading indicator
+  // <sl-icon name="hypnotize"></sl-icon>
+  sessionStorage.setItem("searchResults", JSON.stringify(results));
+  window.location = "./index_results.html";
 }
 
-function replaceJunkWithSpace(junkRidden) {
-  if (Array.isArray(junkRidden)) {
-    let hyphenatedJunk = [];
-    junkRidden.forEach((junk, index) => {
-      // if (index < 100 || index > 175) return;
-      let roundOne = junk.replace(/_/g, " ");
-      hyphenatedJunk.push(roundOne);
-    });
-    return hyphenatedJunk;
-  }
-  return junkRidden;
-}
+function convertToApiCompatible(initialValue, field) {
+  let processedValue = [];
 
-function replaceJunkWithHyphen(junkRidden) {
-  if (Array.isArray(junkRidden)) {
-    let hyphenatedJunk = [];
-    junkRidden.forEach((junk) => {
-      let roundOne = junk.replace(/[/|,|&| |(|)|_]+/g, "-");
-      let roundTwo =
-        roundOne[roundOne.length - 1] === "-"
-          ? roundOne.slice(0, -1)
-          : roundOne;
-      hyphenatedJunk.push(roundTwo);
-    });
-    return hyphenatedJunk;
+  if (Array.isArray(initialValue)) {
+    if (field === "breed") {
+      initialValue.forEach((junk) => {
+        let spaced = junk.replace(/_/g, " ");
+        processedValue.push(spaced);
+      });
+    } else {
+      initialValue.forEach((junk) => {
+        let hyphenated = junk.replace(/[/|,|&| |(|)|_]+/g, "-");
+        const last = hyphenated.length - 1;
+        let trimmed =
+          hyphenated[last] === "-" ? hyphenated.slice(0, -1) : hyphenated;
+        processedValue.push(trimmed);
+      });
+    }
   }
-  return junkRidden;
+  return processedValue;
 }
