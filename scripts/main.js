7ec5c1c246b4e709aacbbe499fb9b7fb62e75e70
Amanda Edmond <e_amy0587@hotmail.com>
2023-03-05 20:30:07 -0400

    debug main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,20 @@
 import data from "./data/animals.js";
 import { fetchAnimals } from "./fetchers.js";
 var submitBtn = document.querySelector("#submit");
-// var formInput = document.querySelector('form');
-// form.addEventListener("submit", fetchAnimals);
+var select = document.querySelectorAll("#petSelect sl-select");
+var formInp = document.querySelectorAll("#petSelect sl-input");
+  
+//http://regexlib.com/REDetails.aspx?regexp_id=122
 
-/*function isvalidCode(postalCode) {
-   return /^(?!.*[DFIOQU])[A-VXY][0-9][A-Z] ?[0-9][A-Z][0-9]$/.test(postalCode);
-}
-
-function isValidZip (zip) {
-
-}
-
-if (!isvalidCode(postalCode)) {
+function isValidCode(location) {
+   return /^((\d{5}-\d{4})|(\d{5})|([A-Z|a-z]\d[A-Z|a-z]\d[A-Z|a-z]\d))$/.test(location);
+  if (!isValidCode(location)) {
     alert ("Please enter A valid postal code")  
-  }
-else{
+  }else{
 fetchAnimals();
-*/
+  }
+}
+
 
 
 function validForm() {
@@ -27,23 +24,42 @@ function validForm() {
 }
 }
 
+  
 
-function saveForm(e) {
+function saveForm (e) {
   e.preventDefault();
-  var select = document.querySelectorAll("#petSelect sl-select");
-  var formInp = document.querySelectorAll("#petSelect sl-input");
-
-for (var i = 0; i < select.length; i++) {
-  var cS = select[i] 
-  localStorage.setItem(cS.id, JSON.stringify(cS.value));
+  
+  for (var i = 0; i < select.length; i++) {
+    var cS = select[i] 
+    localStorage.setItem(cS.id, JSON.stringify(cS.value));
   }
-
-for (var i = 0; i < formInp.length; i++) {
-  var cI = formInp[i] 
-  localStorage.setItem(cI.id, JSON.stringify(cI.value));
+  
+  for (var i = 0; i < formInp.length; i++) {
+    var cI = formInp[i] 
+    localStorage.setItem(cI.id, JSON.stringify(cI.value));
   }
-
 }
 
+  
+function getForm() {
+  
+    for (var i = 0; i < select.length; i++) {
+    var cS = select[i] 
+    var selectForm = JSON.parse(localStorage.getItem(cS.id))
+    if (Array.isArray(selectForm)) {
+      console.log(selectForm)
+    cS.setAttribute("value", selectForm.join(" "));
+      
+  }
 
-submitBtn.addEventListener("click", saveForm)
+     for (var i = 0; i < formInp.length; i++) {
+    var cI = formInp[i] 
+    cI.setAttribute = ("value", JSON.parse(localStorage.getItem(cI.id)));
+   };
+  }
+}
+
+getForm();
+
+
+submitBtn.addEventListener("click", saveForm);
