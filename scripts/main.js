a384706cdc31be320cd0ace7f633fe4475ef0c65
Amanda Edmond <e_amy0587@hotmail.com>
2023-03-06 18:14:33 -0400

    debug main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,63 +3,48 @@ import { fetchAnimals } from "./fetchers.js";
 var submitBtn = document.querySelector("#submit");
 var select = document.querySelectorAll("#petSelect sl-select");
 var formInp = document.querySelectorAll("#petSelect sl-input");
-  
+var loc = document.querySelector("#location");
 //http://regexlib.com/REDetails.aspx?regexp_id=122
-
-function isValidCode(location) {
-   return /^((\d{5}-\d{4})|(\d{5})|([A-Z|a-z]\d[A-Z|a-z]\d[A-Z|a-z]\d))$/.test(location);
-  if (!isValidCode(location)) {
-    alert ("Please enter A valid postal code")  
-  }else{
-fetchAnimals();
-  }
-}
-
-
+var regexp = /^((\d{5}-\d{4})|(\d{5})|([A-Z|a-z]\d[A-Z|a-z]\d[A-Z|a-z]\d))$/;
 
 function validForm() {
-   if (breed === false && size === false && gender === false && age === false && color === false && coat === false) {
-        alert ("You must fill out at least one of the fields before the form can be submited.")
-        return;
-}
+  var isValidCode = regexp.test(loc.value);
+  if (!isValidCode) {
+    alert("Please enter A valid postal code");
+  }
+
 }
 
-  
-
-function saveForm (e) {
-  e.preventDefault();
-  
+function saveForm() {
   for (var i = 0; i < select.length; i++) {
-    var cS = select[i] 
+    var cS = select[i];
     localStorage.setItem(cS.id, JSON.stringify(cS.value));
   }
-  
+
   for (var i = 0; i < formInp.length; i++) {
-    var cI = formInp[i] 
+    var cI = formInp[i];
     localStorage.setItem(cI.id, JSON.stringify(cI.value));
   }
 }
 
-  
 function getForm() {
-  
-    for (var i = 0; i < select.length; i++) {
-    var cS = select[i] 
-    var selectForm = JSON.parse(localStorage.getItem(cS.id))
+  for (var i = 0; i < select.length; i++) {
+    var cS = select[i];
+    var selectForm = JSON.parse(localStorage.getItem(cS.id));
     if (Array.isArray(selectForm)) {
-      console.log(selectForm)
-    cS.setAttribute("value", selectForm.join(" "));
-      
+      cS.setAttribute("value", selectForm.join(" "));
+    }
   }
 
-     for (var i = 0; i < formInp.length; i++) {
-    var cI = formInp[i] 
-    cI.setAttribute = ("value", JSON.parse(localStorage.getItem(cI.id)));
-   };
+  for (var i = 0; i < formInp.length; i++) {
+    var cI = formInp[i];
+    var inputForm = JSON.parse(localStorage.getItem(cI.id));
+    cI.value = inputForm;
   }
 }
-
 getForm();
 
-
-submitBtn.addEventListener("click", saveForm);
+submitBtn.addEventListener("click", () => {
+  validForm();
+  saveForm();
+});
